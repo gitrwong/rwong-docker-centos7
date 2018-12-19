40c65eb682265f6fd7c2367f7b22fd4d970aa808
Rob <rwong.gotechsolutions@gmail.com>
2018-12-19 06:40:16 +0000

    update docker script

--- docker.sh
+++ docker.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-git pull https://github.com/gitrwong/rwong-docker-centos7
+cat << EOF | sudo tee /etc/yum.repos.d/docker1.repo
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum install -y docker-engine
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo usermod -a -G docker vagrant
